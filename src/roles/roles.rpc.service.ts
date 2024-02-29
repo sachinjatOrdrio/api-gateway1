80eab795a4fc06ef630d09741d2c0b5b504435b0
Prajwal056 <prajwal.rao@ordrio.com>
2024-02-29 14:22:53 +0530

    updated env

---
+++ src/roles/roles.rpc.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { QueuesEnum } from 'src/common/enums';
+
+@Injectable()
+export class RolesRpcService {
+  private client: ClientProxy;
+
+  constructor(private readonly configService: ConfigService) {
+    this.client = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [this.configService.get('RABBITMQ_URL')],
+        queue: QueuesEnum.STORES,
+      },
+    });
+  }
+
+  sendRequest(pattern: string, data: any): Observable<any> {
+    return new Observable<any>((observer) => {
+      this.client
+        .send<any, any>(pattern, data) // Pass the pattern as the first argument
+        .subscribe({
+          next: (response) => {
+            observer.next(response);
+            observer.complete();
+          },
+          error: (error) => observer.error(error),
+        });
+    });
+  }
+}
